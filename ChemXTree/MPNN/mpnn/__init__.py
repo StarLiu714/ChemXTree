168a0c8e5e453f19ae0d35432813d8e17e4f59cc
Star <liuxinxin@dp.tech>
2024-04-06 17:40:07 +0800

    update MPNN import subpackage

---
+++ ChemXTree/MPNN/mpnn/__init__.py
@@ -0,0 +1,15 @@
+"""Top-level sub-package for MPNN."""
+
+__author__ = """Star Liu"""
+__email__ = "StarLiu@seas.upenn.edu"
+__version__ = "0.0.1"
+
+from .MPNN_Pipeline import MPNN_Pipeline
+
+__all__ = [
+    "MPNN_Pipeline",
+]
+
+for item in __all__:
+    if hasattr(item, "__module__"):
+        setattr(item, "__module__", __name__)
